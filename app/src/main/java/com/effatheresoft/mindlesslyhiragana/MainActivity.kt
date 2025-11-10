3ecdda3143cac5a56637f12ac170b32441aa9364
FathirFathan <fathirfathanwork@gmail.com>
2025-11-10 16:35:41 +0700

    Revert "feat: Implement HomeScreen and DetailsScreen with navigation"
    This reverts commit a8d916336ff1ce83f9f11813825ed62cb854cad1.

--- app/src/main/java/com/effatheresoft/mindlesslyhiragana/MainActivity.kt
+++ app/src/main/java/com/effatheresoft/mindlesslyhiragana/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.compose.rememberNavController
-import com.effatheresoft.mindlesslyhiragana.ui.navigation.DefaultNavHost
-import com.effatheresoft.mindlesslyhiragana.ui.navigation.Route
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.effatheresoft.mindlesslyhiragana.ui.theme.MindlesslyHiraganaTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,11 +18,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
             MindlesslyHiraganaTheme {
-                DefaultNavHost(navController, Route.Home)
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
 }
 
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MindlesslyHiraganaTheme {
+        Greeting("Android")
+    }
+}
