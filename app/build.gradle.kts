33801cab8079caa9a7d447b87580581684a844a0
FathirFathan <fathirfathanwork@gmail.com>
2025-11-10 16:35:47 +0700

    Revert "feat: Add Navigation Compose and Kotlinx Serialization dependencies."
    This reverts commit dc74666e70fb85c94437927ca28f180ec90df933.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -41,8 +40,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
